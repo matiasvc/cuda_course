c9f014e8ee9123c5138f65f498de71d8bdf90ef2
Matias Christensen <mattivc@gmail.com>
2020-02-29 14:29:37 +0100

    work

--- main.cu
+++ main.cu
@@ -12,6 +12,30 @@ void saxpy(float* a_ptr, float* b_ptr, int N) {
 	a_ptr[thread_index] = a_ptr[thread_index] + b_ptr[thread_index];
 }
 
+__global__ void reduce(float* a_dev_ptr) {
+	extern __shared__ float shared_data[];
+	
+	const unsigned int tid = threadIdx.x;
+	const unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
+	
+	shared_data[tid] = a_dev_ptr[i];
+	__syncthreads();
+	
+	for (unsigned int s = 1; s < blockDim.x; s*=2) {
+		if (tid % (2*s) == 0) {
+			shared_data[tid] += shared_data[tid + s];
+		}
+		
+		__syncthreads();
+	}
+	
+	if (tid == 0) {
+		a_dev_ptr[blockIdx.x] = shared_data[0];
+	}
+	
+}
+
+
 int main() {
 	std::cout << "Hello, World!" << std::endl;
 	
@@ -30,10 +54,9 @@ int main() {
 	void* b_dev_ptr = nullptr;
 	cudaMalloc(&a_dev_ptr, sizeof(a));
 	cudaMemcpy(a_dev_ptr, a.data(), sizeof(a), cudaMemcpyHostToDevice);
-	cudaMalloc(&b_dev_ptr, sizeof(b));
-	cudaMemcpy(b_dev_ptr, b.data(), sizeof(b), cudaMemcpyHostToDevice);
+
 	
-	saxpy<<<10, 100>>>(reinterpret_cast<float*>(a_dev_ptr), reinterpret_cast<float*>(b_dev_ptr), N);
+	reduce<<<10, 100, sizeof(float)*100>>>(reinterpret_cast<float*>(a_dev_ptr));
 	
 	cudaMemcpy(a.data(), a_dev_ptr, sizeof(a), cudaMemcpyDeviceToHost);
 	
@@ -41,6 +64,14 @@ int main() {
 	for (const auto e: a) {
 		std::cout << e << '\n';
 	}
+	
+	float sum = 0;
+	
+	for (int i=0; i < 10; i++) {
+		sum += a.at(i);
+	}
+	
+	std::cout << "Total sum: " << sum << '\n';
 	
 	return 0;
 }
